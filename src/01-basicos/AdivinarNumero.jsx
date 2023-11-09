67d28abe4db549d64fefcc24046274910d2b8eaf
bibs <bibsbiana@protonmail.com>
2023-11-09 11:39:23 +0100

    Actualizado: Basicos, 08-Adivina número

--- src/01-basicos/AdivinarNumero.jsx
+++ src/01-basicos/AdivinarNumero.jsx
@@ -2,22 +2,26 @@ import { useState } from 'react';
 
 export default function AdivinarNumero() {
   const [randomNumber] = useState(Math.floor(Math.random() * 100) + 1);
-  const [number, setNumber] = useState(null);
-  const [result, setResult] = useState(null);
+  const [number, setNumber] = useState('');
+  const [result, setResult] = useState('');
   const [tries, setTries] = useState(0);
 
   console.log('NumRandom:', randomNumber);
   function adivinaElNum() {
     console.log('NumUsuario:', number);
     console.log('Tipo:', typeof randomNumber, typeof number, typeof tries);
-    if (number === randomNumber) {
-      setResult('Has acertado!');
-    } else if (number > randomNumber) {
-      setResult('El número que buscas es menor');
-    } else if (number < randomNumber) {
-      setResult('El número que buscas es mayor');
+    if (isNaN(parseInt(number)) || !number) {
+      setResult('Introduce un número');
+    } else {
+      if (number === randomNumber) {
+        setResult('Has acertado!');
+      } else if (number > randomNumber) {
+        setResult('El número que buscas es menor');
+      } else if (number < randomNumber) {
+        setResult('El número que buscas es mayor');
+      }
+      setTries(tries + 1);
     }
-    setTries(tries + 1);
   }
 
   return (
@@ -37,6 +41,8 @@ export default function AdivinarNumero() {
         >
           Adivina el número
         </button>
+
+        <button onClick={() => location.reload()}>Reinicia</button>
         <p id="resultado">{result}</p>
         <p id="intentos">Has usado {tries} intentos</p>
       </div>
