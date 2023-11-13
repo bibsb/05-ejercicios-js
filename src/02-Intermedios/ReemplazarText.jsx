3cb8901faa982fa405d1d53c1d7cd30b98274ed9
bibs <bibsbiana@protonmail.com>
2023-11-13 19:05:17 +0100

    Añadido: Intermedio, 03-Reemplazador de texto

--- src/02-Intermedios/ReemplazarText.jsx
+++ src/02-Intermedios/ReemplazarText.jsx
@@ -1,37 +1,89 @@
 import { useState } from 'react';
 
 export default function ReemplazarText() {
+  const [sentence, setSentence] = useState('');
+  const [oldWord, setOldWord] = useState('');
+  const [newWord, setNewWord] = useState('');
+  const [result, setResult] = useState('');
+
+  const replaceWord = () => {
+    const words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      if (words[i] === oldWord) {
+        words[i] = newWord;
+      }
+    }
+
+    setResult(words.join(' '));
+  };
+
   return (
     <>
       <div className="container" id="contenedor-basico">
-        <h3>03 - Reemplazador de texto</h3>
-        {/* <p>Busca el número entre 1 y 100</p> */}
-        <div className="input-group mb-3 ">
-          <input
-            type="number"
+        <h3>3 - Reemplazador de texto</h3>
+
+        <div className="form-floating mb-3">
+          <textarea
             className="form-control"
-            placeholder="Introduce un número..."
-            // value={number}
-            // onChange={(e) => setNumber(parseInt(e.target.value, 10))}
+            placeholder="Introduce una frase..."
+            value={sentence}
+            onChange={(e) => setSentence(e.target.value)}
+            id="floatingTextarea2"
+            style={{ height: '100px' }}
           />
-          <button
-            className="btn btn-outline-secondary btn-sm"
-            type="button"
-            id="button-addon2"
-            // onClick={() => adivinaElNum()}
-            // disabled={
-            //   number === '' || tries === 0 || result === 'Has acertado!'
-            // }
-          >
-            Adivina el número
-          </button>
+          <label htmlFor="floatingTextarea2">Introduce una frase...</label>
         </div>
 
-        {/* <p id="resultado">{result}</p>
-        <p id="intentos">Te quedan {tries} intentos</p> */}
+        <div className="row">
+          <div className="col-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Introduce la palabra a cambiar..."
+              value={oldWord}
+              onChange={(e) => setOldWord(e.target.value)}
+            />
+          </div>
+
+          <div className="col-7">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Introduce la palabra nueva..."
+                value={newWord}
+                onChange={(e) => setNewWord(e.target.value)}
+              />
+              <button
+                className="btn btn-outline-secondary btn-sm"
+                type="button"
+                id="button-addon2"
+                onClick={() => replaceWord()}
+                disabled={!oldWord || !newWord}
+              >
+                Reemplazar palabra
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {result !== '' && (
+          <div>
+            <p>Frase modificada:</p>
+            <p style={{ textAlign: 'right'}}>
+              <b>{result}</b>
+            </p>
+          </div>
+        )}
         <button
           className="btn btn-outline-secondary btn-sm"
-          //   onClick={() => setNumber('')}
+          onClick={() => {
+            setSentence('');
+            setResult('');
+            setOldWord('');
+            setNewWord('');
+          }}
         >
           Reiniciar
         </button>
